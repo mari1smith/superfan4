1e76c0ed9e489f0c04d848d409209cc2b50e9c1d
mari1smith <amari_smith@davincischools.org>
2017-12-13 00:06:03 -0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+
+function playSound1(e){
+	document.getElementById("late").play();
+	document.getElementById("late2").pause();
+}
+
+function playSound2(e){
+	document.getElementById("late2").play();
+	document.getElementById("late1").pause();
+}
