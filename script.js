c4170126417101e9f51b04ebf4e4ee37b9c6450e
mari1smith <amari_smith@davincischools.org>
2017-12-13 10:41:39 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 
-function playSound1(e){
+function playSound1(elem){
 	document.getElementById("late").play();
 	document.getElementById("late2").pause();
 }
 
-function playSound2(e){
+function playSound2(elem){
 	document.getElementById("late2").play();
 	document.getElementById("late1").pause();
 }
 
-function change2009(e) {
+function change2009(elem) {
 	document.getElementById("pink").classList.toggle("2009");
 }
