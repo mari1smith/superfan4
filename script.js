c2e78522e67cf3d045b4686b6a842c1d82b84247
mari1smith <amari_smith@davincischools.org>
2017-12-13 11:46:12 -0800

    Update script.js

--- script.js
+++ script.js
@@ -10,17 +10,29 @@ function playSound2(elem){
 }
 
 function change2009(elem) {
-	document.getElementById("pink").classList.toggle("2009");
+	document.getElementById("pink").classList.remove("2010");
+	document.getElementById("pink").classList.remove("2011");
+	document.getElementById("pink").classList.remove("2012");
+document.getElementById("pink").classList.toggle("2009");
 }
 
 function change2010(elem) {
-	document.getElementById("pink").classList.toggle("2010");
+	document.getElementById("pink").classList.remove("2009");
+	document.getElementById("pink").classList.remove("2011");
+	document.getElementById("pink").classList.remove("2012");
+document.getElementById("pink").classList.toggle("2010");
 }
 
 function change2011(elem) {
-	document.getElementById("pink").classList.toggle("2011");
+	document.getElementById("pink").classList.remove("2010");
+	document.getElementById("pink").classList.remove("2009");
+	document.getElementById("pink").classList.remove("2012");
+document.getElementById("pink").classList.toggle("2011");
 }
 
 function change2012(elem) {
-	document.getElementById("pink").classList.toggle("2012");
+	document.getElementById("pink").classList.remove("2010");
+	document.getElementById("pink").classList.remove("2011");
+	document.getElementById("pink").classList.remove("2009");
+document.getElementById("pink").classList.toggle("2012");
 }
